23ee4144ab68f6592004466f954ea9080db6e760
kangsan.fwj <fuwenjun.fwj@alibaba-inc.com>
2024-08-08 02:45:55 +0000

    prefill rank by last digit in port

--- vllm/splitwise/request.py
+++ vllm/splitwise/request.py
@@ -3,5 +3,7 @@ class SplitwiseRequest:
         self.prefill_endpoint = prefill_endpoint
         self.decoding_rank = decoding_rank
         # FIXME: determine prefill_rank by endpoint
-        self.prefill_rank = 0 if prefill_endpoint else None
+        if prefill_endpoint:
+            port = int(prefill_endpoint.split(':')[-1].split('/')[0])
+        self.prefill_rank = port % 10 if prefill_endpoint else None
         self.future = None
